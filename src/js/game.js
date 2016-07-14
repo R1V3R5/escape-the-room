7a022728567781c4646de0f2c53e20edf6a8c4f4
Joshua Rivers <joshua.rivers@gmail.com>
2016-07-14 15:11:29 -0400

    have 4 test written and passing

---
+++ src/js/game.js
@@ -0,0 +1,9 @@
+import { paths } from './path'
+
+class Game {
+  constructor (path) {
+    this.path = path || [];
+  }
+}
+
+export { Game };
--- test/test.js
+++ test/test.js
@@ -2,22 +2,40 @@
 import chai from 'chai';
 
 // Import Any Files to Test
-
+import { Game } from '../src/js/game';
+import { Player } from '../src/js/player';
 // Set Chai Constants
 const expect = chai.expect;
 const should = chai.should();
 const assert = chai.assert;
 
-describe('Something We Want To Test', function () {
 
-  describe('Testing the Creation of Something', function () {
+// Test our Game Class
+describe('About our Game', () => {
 
-    it('should exist after we create it', function () {
-      let x = new Object;
-      expect(x).to.be.an.instanceof(Object);
-    });
-
+  let game;
+  beforeEach(() => {
+    game = new Game();
   });
 
+  describe('Creation of our Game', () => {
+    it('should be an instance of Game', () => {
+      expect(game).to.be.an.instanceof(Game);
+    });
+    it('path should be an array', () => {
+      expect(game.path).to.be.an('array');
+    });
+  });
+
+});
+
+describe('About our Player', () => {
+
+  describe('Creation of player', () => {
+    it('should be an instance of Player', () => {
+      let player = new Player();
+      expect(player).to.be.an.instanceof(Player);
+    });
+  });
 
 });
